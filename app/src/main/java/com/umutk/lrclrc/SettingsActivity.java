cad060e56a846512e43018b20e119969c4b4439c
root <root@localhost.localdomain>
2026-06-18 15:03:29 +0300

    fix: remove unnecessary BuildConfig import

--- app/src/main/java/com/umutk/lrclrc/SettingsActivity.java
+++ app/src/main/java/com/umutk/lrclrc/SettingsActivity.java
@@ -17,7 +17,6 @@ import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.button.MaterialButtonToggleGroup;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.umutk.lrclrc.BuildConfig;
 import com.google.android.material.materialswitch.MaterialSwitch;
 import com.google.android.material.slider.Slider;
 import com.google.android.material.textfield.TextInputEditText;
